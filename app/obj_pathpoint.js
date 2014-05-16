7235173bb4a906a3f912575d23fee3ef47dd5a97
mattlag <mattlag@gmail.com>
2014-05-16 13:51:51 -0700

    Removed COORD.js

--- app/obj_pathpoint.js
+++ app/obj_pathpoint.js
@@ -486,3 +486,17 @@
 			_UI.chareditctx.stroke();
 		}
 	};
+
+	
+//-------------------------------------------------------
+// COORDINATE OBJECT
+//-------------------------------------------------------
+
+	function Coord(oa){
+		this.objtype = "coord";
+
+		this.x = oa.x || 0;
+		this.y = oa.y || 0;
+		this.xlock = oa.xlock || false;
+		this.ylock = oa.yllock || false;
+	}
