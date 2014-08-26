9aae261e342ea27ca43dfc6a64a0b2f0d8eb0b5d
mattlag <mattlag@gmail.com>
2014-08-25 22:39:54 -0700

    Grunt!

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				wrap: 'Glyphr_Studio',
+				preserveComments: false
+			},
+			build: {
+				src: 'Glyphr_Studio.js',
+				dest: 'Glyphr_Studio.min.js'
+			}
+		},
+		concat: {
+			build: {
+				src: ['js/*.js'],
+				dest: 'Glyphr_Studio.js'
+			}
+		}
+	});
+
+	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Tasks
+	grunt.registerTask('default', ['concat', 'uglify']);
+
+};
