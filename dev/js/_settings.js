c511cebe049db3eee8887660b53bb2cffa7ca7c5
mattlag <mattlag@gmail.com>
2015-05-18 12:17:51 -0700

    v1.00.05

--- dev/js/_settings.js
+++ dev/js/_settings.js
@@ -13,7 +13,7 @@
 // These are NOT SAVED with the project
 	var _UI = {
 		// Internal Dev Stuff
-		'devmode': true,		// global switch for all the stuff below
+		'devmode': false,		// global switch for all the stuff below
 		'debug': true,			// show messages sent to the browser console
 		'loadsampleproject': false,	// if sampleproject is present, load it and skip open project experience
 		'devnav': false,		// navigate straight to a page
