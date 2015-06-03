e05ea5adcd8a71b9675051a18e854a21497845fc
mattlag <mattlag@gmail.com>
2015-06-03 15:11:39 -0700

    Export Font Page fit and finish

--- dev/js/page_exportfont.js
+++ dev/js/page_exportfont.js
@@ -19,16 +19,16 @@
 			'</ul>'+
 			'If you want to retain these features, be sure to also save a Glyphr Studio Project File.<br><br>';
 
-		content += '<table><tr><td style="padding-top:10px;">'+makeIcon({'name':'nav_exportotf', 'size':60, 'color':_UI.colors.blue.l55, 'hovercolor':false})+'</td><td style="padding-left:20px;">'+
+		content += '<table><tr><td style="padding-top:10px;">'+makeIcon({'name':'nav_exportotf', 'size':60, 'color':_UI.colors.blue.l35, 'hovercolor':false})+'</td><td style="padding-left:10px;">'+
 			'</td><td><h2>OTF Font</h2>'+
-			'Open Type Fonts are font files that can be installed on any computer, then used for things like word processing or graphics.<br><br>'+
-			'<button class="buttonsel" onclick="ioOTF_exportOTFfont();">Export OTF Font</button>'+
+			'Open Type Fonts are font files that can be installed on any computer, then used for things like word processing or graphics.<br>'+
+			'<button class="buttonsel" style="margin-top:10px;" onclick="ioOTF_exportOTFfont();">Export OTF Font</button>'+
 			'</td></tr></table>';
 
-		content += '<br><table><tr><td style="padding-top:10px;">'+makeIcon({'name':'nav_exportsvg', 'size':60, 'color':_UI.colors.blue.l55, 'hovercolor':false})+'</td><td style="padding-left:20px;">'+
+		content += '<br><br><table><tr><td style="padding-top:10px;">'+makeIcon({'name':'nav_exportsvg', 'size':60, 'color':_UI.colors.blue.l35, 'hovercolor':false})+'</td><td style="padding-left:10px;">'+
 			'</td><td><h2>SVG Font</h2>'+
-			'Scalable Vector Graphics Fonts are text-file-based fonts that some browsers can use natively to display web page text.  You can also convert SVG Fonts to any other kind of font using free online services.<br><br>'+
-			'<button class="buttonsel" onclick="ioSVG_exportSVGfont();">Export SVG Font</button>'+
+			'Scalable Vector Graphics Fonts are text-file-based fonts that some browsers can use natively to display web page text.  You can also convert SVG Fonts to any other kind of font using free online services.<br>'+
+			'<button class="buttonsel" style="margin-top:10px;" onclick="ioSVG_exportSVGfont();">Export SVG Font</button>'+
 			'</td></tr></table><br><br><br>';
 
 		content += '</div>';
