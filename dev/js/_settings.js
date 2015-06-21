b4660fa211e621c92720f93ae7a54f310aa1acd0
mattlag <mattlag@gmail.com>
2015-06-21 14:18:21 -0700

    v1.01.03

--- dev/js/_settings.js
+++ dev/js/_settings.js
@@ -13,10 +13,10 @@
 // These are NOT SAVED with the project
 	var _UI = {
 		// Internal Dev Stuff
-		'devmode': true,		// global switch for all the stuff below
+		'devmode': false,		// global switch for all the stuff below
 		'debug': true,			// show messages sent to the browser console
 		'loadsampleproject': false,	// if sampleproject is present, load it and skip open project experience
-		'devnav': 'import svg',		// navigate straight to a page
+		'devnav': false,		// navigate straight to a page
 		'devnavprimary': false, // navigate straight to a panel
 		'devselectedshape': false, // automatically select a shape
 		// SelectedGlyph
