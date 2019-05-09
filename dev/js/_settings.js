c0291cd5e6c3c031514656c64ee8c3a1eb65d0e5
Matt LaGrandeur <mattlag@users.noreply.github.com>
2019-05-09 15:17:43 -0700

    v1.13

--- dev/js/_settings.js
+++ dev/js/_settings.js
@@ -17,12 +17,12 @@
 		// Version
 		thisGlyphrStudioVersion: 'Version 1.13',
 		thisGlyphrStudioVersionNum: '1.13.00',
-		thisGlyphrStudioVersionDate: 0,
+		thisGlyphrStudioVersionDate: 1557450000000,
 
 		// Internal Dev Stuff
-		devmode: true, 					// global switch for all the stuff below
+		devmode: false, 					// global switch for all the stuff below
 		dev_sample_project: false, 		// load a sample project and skip open project experience
-		dev_current_page: 'glyph edit',		// navigate straight to a page
+		dev_current_page: false,		// navigate straight to a page
 		dev_current_panel: false, 		// navigate straight to a panel
 		dev_selected_shape: false, 		// automatically select a shape
 		debug: true, 					// show messages sent to the browser console
